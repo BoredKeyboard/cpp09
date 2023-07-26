b47788a70fdd72681ebfb50102502e23c0cb7aec
Milan Forsthövel <mforstho@student.codam.nl>
2023-07-26 16:58:27 +0200

    btc cleaned up, but still need to catch a few edgecases

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 16:00:56 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/07/26 16:16:03 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/07/26 16:26:30 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,48 @@ bool	BitcoinExchange::is_valid_date(int y, int m, int d) {
 	return (true);
 }
 
-// void	BitcoinExchange::save_data() {
-// 	std::string	line;
+std::map<std::string, float>	BitcoinExchange::save_data(std::string data) {
+	std::map<std::string, float> datamap;
+	std::ifstream	inputdata(data);
+	std::string	line;
 
-// 	getline(inputdata, line);
-// 	while (getline(inputdata, line)) {
-// 		std::string	date = line.substr(0, line.find(","));
-// 		float	value = atof((line.substr(line.find(",") + 1, line.find("\n"))).c_str());
-// 		datamap.insert(std::pair<std::string,float>(date, value));
-// 	}
-// }
+	getline(inputdata, line);
+	while (getline(inputdata, line)) {
+		std::string	date = line.substr(0, line.find(","));
+		float	value = atof((line.substr(line.find(",") + 1, line.find("\n"))).c_str());
+		datamap.insert(std::pair<std::string,float>(date, value));
+	}
+	return (datamap);
+}
+
+void	BitcoinExchange::find_data(std::string file, std::map<std::string, float> datamap) {
+	std::ifstream inputfile(file);
+	std::string	line;
+	getline(inputfile, line);
+	while (getline(inputfile, line)) {
+		if (!is_valid_date(atoi((line.substr(0, 4)).c_str()), atoi((line.substr(5, 2)).c_str()), atoi((line.substr(8, 2)).c_str()))) {
+			std::cout << "Error: bad input => " << line << std::endl;
+		}
+		else if (line.find(" | ") == line.npos) {
+			std::cout << "Error: wrong format => " << line << std::endl;
+		}
+		else {
+			std::string	date = line.substr(0, line.find(" | "));
+			std::string	value = line.substr(line.find(" | ") + 3, line.find("\n"));
+			if (value.c_str()[0] == '-') {
+				std::cout << "Error: not a positive number." << std::endl;
+			}
+			else if ((unsigned int)std::atoi(value.c_str()) > 1000){
+				std::cout << "Error: too large a number." << std::endl;
+			}
+			else {
+				std::map<std::string,float>::iterator it;
+				it = --datamap.upper_bound(date);
+				// std::cout << "Date: " << date << "    Value: " << value << std::endl;
+				// std::cout << "Date: " << it->first << "    Value: " << it->second << std::endl;
+				std::cout << date << " => " << value << " = " << (it->second * atof(value.c_str())) << std::endl;
+			}
+		}
+		// std::cout << std::endl;
+	}
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 16:00:53 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/07/26 16:15:20 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/07/26 16:26:48 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@
 
 class BitcoinExchange {
 	private:
-		std::map<std::string, float>	_datamap;
 		BitcoinExchange(void);
 		BitcoinExchange(BitcoinExchange const & src);
 		virtual ~BitcoinExchange(void);
@@ -31,6 +30,8 @@ class BitcoinExchange {
 
 	public:
 		static bool is_valid_date(int y, int m, int d);
+		static std::map<std::string, float>	save_data(std::string data);
+		static void	find_data(std::string file, std::map<std::string, float> datamap);
 };
 
 #endif
