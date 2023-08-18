892b8a214ca305ecc1d5759769195dc5bf424df0
Milan Forsthövel <mforstho@student.codam.nl>
2023-08-18 14:42:11 +0200

    Cleaned up main for ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 16:00:10 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/07/27 13:31:38 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/08/18 14:41:37 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,5 @@ int	main(int argc, char *argv[]) {
 	}
 	std::map<std::string, float>	datamap;
 	datamap = BitcoinExchange::save_data("data.csv");
-
-			// to display the saved map
-	// std::map<std::string,float>::iterator it=datamap.begin();
-	// for (int i = 0; i < 3000; i++) {
-   	// 	std::cout << it->first << " => " << it->second << std::endl;
-	// 	it++;
-	// 	if (it == datamap.end())
-	// 		break ;
-	// }
 	return (BitcoinExchange::find_data(argv[1], datamap));
 }
